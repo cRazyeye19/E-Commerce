a96e1551e9f3adfb95dced7a79d856e679e94f96
John Lester Pansoy <jlesterpansoy@gmail.com>
2024-09-16 20:46:18 +0800

    First Commit

---
+++ src/main/java/com/group3sd3/E_commerce/ECommerceApplication.java
@@ -0,0 +1,13 @@
+package com.group3sd3.E_commerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommerceApplication.class, args);
+	}
+
+}
