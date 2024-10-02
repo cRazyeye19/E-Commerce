c65bc2dca36f06a1e00e149102072d5517661bda
John Lester Pansoy <jlesterpansoy@gmail.com>
2024-10-02 16:09:13 +0800

    Fixed the issue on category model by adding @Entity annotation

--- E-commerce/src/main/java/com/group3sd3/E_commerce/model/categoryModel.java
+++ E-commerce/src/main/java/com/group3sd3/E_commerce/model/categoryModel.java
@@ -1,5 +1,6 @@
 package com.group3sd3.E_commerce.model;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Getter
 @Setter
+@Entity
 public class categoryModel {
 
     @Id
