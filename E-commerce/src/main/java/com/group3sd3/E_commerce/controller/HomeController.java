34c08d4da026bd6e303d18e3956054f679cda823
John Lester Pansoy <jlesterpansoy@gmail.com>
2024-10-16 23:39:48 +0800

    Implemented dynamic product and category listings

--- E-commerce/src/main/java/com/group3sd3/E_commerce/controller/HomeController.java
+++ E-commerce/src/main/java/com/group3sd3/E_commerce/controller/HomeController.java
@@ -1,11 +1,27 @@
 package com.group3sd3.E_commerce.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.group3sd3.E_commerce.model.Product;
+import com.group3sd3.E_commerce.model.Category;
+import com.group3sd3.E_commerce.service.ProductService;
+import com.group3sd3.E_commerce.service.CategoryService;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/")
     public String index() {
@@ -33,13 +49,20 @@ public class HomeController {
     }
 
     @GetMapping("/products")
-    public String products() {
+    public String products(Model m, @RequestParam(value = "category", defaultValue = "") String category) {
+        // System.out.println("category="+category);
+        List<Category> categories = categoryService.getAllActiveCategory();
+        List<Product> products = productService.getAllActiveProducts(category);
+        m.addAttribute("categories", categories);
+        m.addAttribute("products", products);
+        m.addAttribute("paramValue", category);
         return "product";
     }
 
-    @GetMapping("/product")
-    public String product() {
+    @GetMapping("/product/{id}")
+    public String product(@PathVariable int id, Model m) {
+        Product productById = productService.getProductById(id);
+        m.addAttribute("product", productById);
         return "view_product";
     }
-    
 }
